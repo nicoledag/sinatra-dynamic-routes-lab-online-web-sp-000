4d12541e3ea4bcf18f07b90e5ab16c1ead4bd2da
Nicole D <nnardella@gmail.com>
2019-01-29 16:20:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,9 +30,10 @@ class App < Sinatra::Base
       params[:number2].to_i - params[:number1].to_i
     elsif params[:operation] == "multiply"
       params[:number1].to_i * params[:number2].to_i
+      binding.pry
     else params[:operation] == "divide"
       params[:number1].to_i / params[:number2].to_i
-      binding.pry
+
     end
 
   end
